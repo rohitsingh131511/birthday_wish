ae3716d4a142829e42b193560ba38176652b999b
Rohit Singh <rohitsingh@131511@gmail.com>
2026-07-31 22:21:32 +0530

    fix image

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export type Memory = { src: string; caption: string };
 import img1 from "./images/1.jpg";
 import img2 from "./images/2.jpg";
 import img3 from "./images/3.jpg";
-import img4 from "./images/4.jpg";
+import img4 from "./images/8.jpg";
 import img5 from "./images/5.jpg";
 import img6 from "./images/6.jpg";
 
